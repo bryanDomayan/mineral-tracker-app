98357847b68fba224643aeda5c765ff1a3c56020
cedriccandido451 <cedriccandido451@gmail.com>
2024-10-13 23:10:46 +0800

    fix build error auth login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,18 +1,35 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 import { authorize } from "@/lib/auth";
+
 export async function POST(req: Request) {
-    try {
-        const { email, password } = await req.json()
+  try {
+    const { email, password } = await req.json();
 
-        const auth = await authorize(email, password)
+    const auth = await authorize(email, password);
 
-        if (!auth) {
-            return NextResponse.json({message: "UNAUTHORIZED"}, { status: 401 });
-        }
-        cookies().set('serverToken', auth.token);
-        return Response.json({message: 'success', data: { token: auth.token, ...auth.user } })
-    } catch (error) {
-        return Response.json(error, { status: 500 })
+    if (!auth) {
+      return NextResponse.json({ message: "UNAUTHORIZED" }, { status: 401 });
     }
+
+    // Setting the cookie using NextResponse
+    const response = NextResponse.json({
+      message: "success",
+      data: { token: auth.token, ...auth.user },
+    });
+
+    response.cookies.set("serverToken", auth.token, {
+      httpOnly: true,
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
